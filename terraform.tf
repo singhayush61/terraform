f8c058aff33c30a888997957bcbb0e7e0819b348
singhayush61 <ayushofficialsv@gmail.com>
2026-04-07 22:57:30 +0530

    feat| update provider versions in .terraform.lock.hcl and terraform.tf to latest compatible releases

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
     }
   }
 }
