638e38dd7cef9bde4e0fbf8c0ce3c68b41bff049
singhayush61 <ayushofficialsv@gmail.com>
2026-04-08 14:29:19 +0530

    feat| add server module for EC2 instance creation; refactor main.tf to utilize the new module

--- main.tf
+++ main.tf
@@ -152,3 +152,14 @@ resource "aws_instance" "ubuntu_server" {
     Owner = local.team
   }
 }
+
+module "server" {
+  source          = "./modules/server"
+  ami             = data.aws_ami.ubuntu.id
+  subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
+  security_groups = [
+    aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+}
