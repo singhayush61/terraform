47ed90be276999456976bd2067088492000f3818
Ayush Singh <160009745+singhayush61@users.noreply.github.com>
2026-04-29 18:54:16 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,10 @@ terraform {
       version = "~> 5.0"
     }
 
-
     http = {
       source  = "hashicorp/http"
       version = "2.1.0"
     }
-
 
     random = {
       source  = "hashicorp/random"
